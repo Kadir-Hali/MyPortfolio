18a9cc3bc8831ea8a4fde466b81540cd70d3350c
akadirhali <kadirhali07@gmail.com>
2023-12-01 17:35:34 +0300

    Development Guide

---
+++ MyPortfolio/Controllers/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        MyPortfolioDbEntities1 db = new MyPortfolioDbEntities1();
+        public ActionResult Index()
+        {
+            var values = db.Service.ToList();
+            return View(values);
+        }
+    }
+}
