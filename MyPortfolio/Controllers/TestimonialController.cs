f0a86c17374cad40648235de9718a4122bb66ebd
akadirhali <kadirhali07@gmail.com>
2023-12-14 15:24:15 +0300

    Development Guide

---
+++ MyPortfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddTestimonial()
+        {
+            return View();
+        }
+    }
+}
