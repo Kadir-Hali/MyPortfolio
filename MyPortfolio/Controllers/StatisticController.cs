d6343189204c05e601d83555f0762ff5f266dca8
akadirhali <kadirhali07@gmail.com>
2024-01-15 23:34:18 +0300

    Development Guide

--- MyPortfolio/Controllers/StatisticController.cs
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -19,6 +19,8 @@ namespace MyPortfolio.Controllers
             ViewBag.avgPrice = db.Project.Average(x => x.Price);
             var value = db.Project.Max(x => x.Price);
             ViewBag.maxPriceProject = db.Project.Where(x => x.Price == value).Select(y=>y.Title).FirstOrDefault();
+            var value2 = db.Category.Where(x=>x.CategoryName== "AspNet Core Web Geliştirme").Select(y=>y.CategoryID).FirstOrDefault();
+            ViewBag.categoryCountByName = db.Project.Where(x=>x.ProjectCategory == value2).Count();
             return View();
         }
     }
